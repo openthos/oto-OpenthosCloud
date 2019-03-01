b61396c57f45c8776dd76beb3fa7919a649ecea8
dongpeng <dongpeng@openthos.org>
2019-03-01 13:03:09 +0800

    OpenthosCloud Version 2019-01-24

---
+++ app/src/main/java/com/seafile/seadroid2/ui/activity/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.seafile.seadroid2.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+import com.seafile.seadroid2.R;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_activiy);
+
+
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(1000);
+                    startActivity(new Intent(SplashActivity.this,BrowserActivity.class));
+                    SplashActivity.this.finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                super.run();
+            }
+        }.start();
+    }
+}
